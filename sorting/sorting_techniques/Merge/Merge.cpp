82c1e6fac1c38d627db020c76a99d03c73b2d897
iankit3 <a4ankit@outlook.com>
2019-08-22 01:02:14 +0530

    labelled IMPL

--- sorting/sorting_techniques/Merge/Merge.cpp
+++ sorting/sorting_techniques/Merge/Merge.cpp
@@ -14,7 +14,7 @@ vector<int>* merge(const vector<int>& vec1, const vector<int>& vec2) {
     // Size of the smallest vec
     int max_size = vec1.size() > vec2.size() ? vec1.size() : vec2.size();
     vector<int> vec{};
-    //INCORRECT
+    // INCORRECT
     for ( int i = 0, j = 0; i < max_size && j < max_size ; ) {
         if (vec1.at(i) <= vec2.at(j)) {
             vec.push_back(vec1.at(i));
