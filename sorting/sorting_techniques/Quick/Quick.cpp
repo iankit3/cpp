33fee610e9b4840ee5d33b6d1cfa86a30f879643
iankit3 <a4ankit@outlook.com>
2019-09-03 22:04:53 +0530

    added comments/debugging logs

--- sorting/sorting_techniques/Quick/Quick.cpp
+++ sorting/sorting_techniques/Quick/Quick.cpp
@@ -16,17 +16,23 @@ void swap(vector<int>* vec, int x, int y) {
 }
 
 int partition(vector<int>* vec, int start, int end) {
+    // choose the first elem as pivot
     int pivot = vec->at(start);
+    // track the pivot
     int i = start;
 
     for (int j = start; j < end; j++) {
-        if (vec->at(j) <= pivot) {
-            swap(vec, i, j);
+        /* cout << "Pivot="<< pivot <<", i="<< i << ", vec.at(j)=" */
+        /* cout << vec->at(j) << endl;*/
+        // SWAP to insert vec[j] after pivot
+        if (vec->at(j) < pivot) {
             i++;
+            swap(vec, i, j);
         }
     }
-
+    // SWAP the last smallest elem (vec[i]) with pivot
     swap(vec, i, start);
+    /* print_vec(*vec); */
 
     return i;
 }
@@ -34,7 +40,10 @@ int partition(vector<int>* vec, int start, int end) {
 void quick_sort(vector<int>* unsorted_vec, int start, int end) {
     if (start < end) {
         int pivot_index = partition(unsorted_vec, start, end);
-        quick_sort(unsorted_vec, start, pivot_index-1);
+        /* cout << "Pivot index= " << pivot_index << endl; */
+        // vec.slice, Excluding pivot elem
+        quick_sort(unsorted_vec, start, pivot_index);
+        // vec.slice, Excluding pivot elem
         quick_sort(unsorted_vec, pivot_index+1, end);
     }
 }
